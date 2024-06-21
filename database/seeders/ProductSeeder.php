35736f6d0582788091851c412b8c70e08089f9d5
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2024-06-21 23:00:53 +0900

    created model -a api

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('products')->insert([
+          'nama' => Str::random(10),
+          'desc' => Str::random(10),
+          'price' => 5000,
+      ]);
+    }
+}
