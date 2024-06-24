b18aedc9c78612d81c9a5e7fd068550395f36a6f
fhwattiheluw <fadliwattiheluw1994@gmail.com>
2024-06-24 19:12:57 +0900

    Relationship created

--- app/Models/review.php
+++ app/Models/review.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function Product()
+    {
+      return $this->belongsTo(product::class);
+    }
 }
